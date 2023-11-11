4f032cae38aefb9de568bc3e12ba3653d4fbeb78
BLAKE RAPHAEL <braphael99@gmail.com>
2023-11-11 12:28:24 -0700

    Added alter.sql and foreign key constraints

--- All Project Code and Components/init_data/create.sql
+++ All Project Code and Components/init_data/create.sql
@@ -6,7 +6,6 @@ CREATE TABLE customers(
   username VARCHAR(1000),
   password VARCHAR(1000),
   funds_avail DECIMAL(3,2),
-  items_in_cart INT,
   favorite_type VARCHAR(1000),
   cart_id INT
 );
@@ -26,10 +25,10 @@ CREATE TABLE orders(
   order_id SERIAL PRIMARY KEY,
   order_number INT,
   order_date DATE,
-  shipping_address VARCHAR(100),
-  shipping_city VARCHAR(50),
-  shipping_state VARCHAR(50),
-  shipping_country VARCHAR(50),
+  shipping_address VARCHAR(1000),
+  shipping_city VARCHAR(1000),
+  shipping_state VARCHAR(1000),
+  shipping_country VARCHAR(1000),
   shipping_zip INT,
   cart_id INT
 );
@@ -43,8 +42,9 @@ CREATE TABLE order_lines(
 
 CREATE TABLE items(
   item_id SERIAL PRIMARY KEY,
-  item_name VARCHAR(50),
-  item_type VARCHAR(50),
-  item_description VARCHAR(100),
-  item_price DECIMAL(3,2)
+  item_name VARCHAR(1000),
+  item_type VARCHAR(1000),
+  item_description VARCHAR(1000),
+  item_price DECIMAL(3,2),
+  item_image_link VARCHAR(1000)
 );
