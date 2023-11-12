5d4cfc8b879d4b2ceaecf9b71b6f6f9feea85704
BLAKE RAPHAEL <braphael99@gmail.com>
2023-11-12 10:42:34 -0700

    Added 6 starter hats in our insert.sql and updated team meeting logs for next meeting

---
+++ All Project Code and Components/init_data/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO items (item_id, item_name, item_type, item_description, item_price, item_image_link)
+  VALUES (DEFAULT, 'fedora', 'dress', 'A classic fedora hat mlady', 15.00, 'https://upload.wikimedia.org/wikipedia/commons/9/93/A_fedora_hat%2C_made_by_Borsalino.jpg'),
+  (DEFAULT, 'snapback', 'casual', 'A flatbrim hat fit to you', 20.00, 'https://m.media-amazon.com/images/I/61It33zI5rL._AC_SX679_.jpg'),
+  (DEFAULT, 'cowboy', 'costume', 'A hat to conquer the wild west', 25.00, 'https://s7.orientaltrading.com/is/image/OrientalTrading/PDP_VIEWER_IMAGE/adults-brown-cowboy-hat-with-hatband~ur30030bn'),
+  (DEFAULT, 'beret', 'dress', 'A french classic, very artistic', 20.00, 'https://i5.walmartimages.com/seo/TopHeadwear-100-Wool-Beret-Hat-Cap-Black_409d2b32-ed99-4152-a66c-aae12831b1c7_1.f4756059da30205d1012f811409c4eaa.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF'),
+  (DEFAULT, 'sombrero', 'costume', 'A hat for when you are feeling south of the border', 20.00, 'https://cdn11.bigcommerce.com/s-3stx4pub31/products/6145/images/17495/sombrero-mexicano-con-borlas-adultos-1__53751.1654027564.386.513.jpg?c=2'),
+  (DEFAULT, 'propeller hat', 'costume', 'A hat fit for the first grade', 15.00, 'https://i.ebayimg.com/images/g/EpsAAOSwmPpg-C1B/s-l1200.webp');
